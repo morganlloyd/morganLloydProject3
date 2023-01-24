671240a1ac1b3b33f39c245046c0c70f021267e4
Morgan <55657946+morganlloyd@users.noreply.github.com>
2023-01-23 19:21:51 -0700

    imported react-switch and worked on the logic for the light / dark theme toggle function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
 import './index.css'
-
 import AffirmationWall from './components/AffirmationWall';
+import ReactSwitch from 'react-switch';
+import { createContext, useState} from 'react';
+
+export const ThemeContext = createContext(null);
 
 function App() {
+
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((current) => (current === "light" ? "dark" : "light"));
+  }
+
   return (
     
-    <>
-    <header className='wrapper'>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
 
-      <h1>Affirm <br/> <span className='titleStyles'>Believe</span> <br/> Recieve</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, ducimus? Quasi facilis veritatis doloribus impedit vero expedita est earum rem facere sequi accusamus!</p>
-    </header>
+      <div className="app" id={theme}>
 
-    <AffirmationWall />
-    </>
+        <header className='wrapper'>
+
+          <div className='themeSwitch'>
+            <label>
+              <span class="material-symbols-outlined">
+                sunny 
+              </span>
+            </label>
+            <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} checkedIcon={false} uncheckedIcon={false} onColor={"#1111"}/>
+            <label>
+              <span class="material-symbols-outlined">
+                dark_mode
+              </span>
+            </label>
+          </div>
+
+          <h1>Affirm <br/> 
+          <span className='titleStyles'>Believe</span>
+          <br/> Recieve</h1>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, ducimus? Quasi facilis veritatis doloribus impedit vero expedita est earum rem facere sequi accusamus!</p>
+
+        </header>
+
+        <AffirmationWall />
+
+      </div>
+
+    </ThemeContext.Provider>
   );
 }
 
