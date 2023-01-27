befe5b1bbf1911ea0152909b1447cef24655380e
Morgan <55657946+morganlloyd@users.noreply.github.com>
2023-01-26 17:50:17 -0700

    Problem solved and implemented logic so that a user is not able to submit an empty form

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,19 +8,30 @@ const Form = () => {
 
     const [affirmationInput, setAffirmationInput] = useState("");
 
+    const [error,setError] = useState(false);
+
     const handleChange = (e) => {
         setAffirmationInput(e.target.value)
+
     }
 
     const handleSubmit = (e) => {
+
         e.preventDefault();
 
+        if (affirmationInput.length === 0) {
+            setError(true)
+            
+        } else {
+        setError(false)
         const db = getDatabase(firebase);
         const dbRef = ref(db);
 
         push(dbRef, affirmationInput);
 
         setAffirmationInput("");
+        }
+
     }
 
 
@@ -33,14 +44,16 @@ const Form = () => {
                     enter a affirmation that inspires you below
                 </label> */}
 
-                <input type="text" name="affirmationInput" onChange= {handleChange} value={affirmationInput} placeholder="ie. i am a successful front-end dev!" autoComplete="off" maxLength={120}/>
+                <input type="text" name="affirmationInput" onChange={handleChange} value={affirmationInput} placeholder="ie. i am a successful front-end dev!" autoComplete="off" maxLength={120}/>
+
+                {error ? <span>input can not be blank</span> : ""}
                 
-                
-                <button className="submitBtn" disabled={!affirmationInput}>submit</button>
+                <button className="submitBtn">submit</button>
             </form>
 
         </section>
     )
 }
 
+// disabled={!affirmationInput}
 export default Form;
